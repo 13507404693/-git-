6c6671c6e69d03e892e8c45ef447a87561a7db3c
13507404693 <13507404693@163.com>
2019-02-24 21:58:11 +0800

    first update

---
+++ config.js
@@ -0,0 +1,10 @@
+
+[remote "origin"]
+    url = https://github.com/13507404693/vcod-git.git
+    fetch = +refs/heads/*:refs/remotes/origin/*
+[branch "master"]
+    remote = origin
+    merge = refs/heads/master
+[user]
+    email =13507404693@163.com
+    name = 13507404693
